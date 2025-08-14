b02fca458431c71541327c3cb84806a1398fdc0b
saac <serch1.alc@gmail.com>
2025-08-14 15:00:43 -0600

    Notebooks de Introdución a la programación en Python

---
+++ 1_ProgramacionPython/miBiblio.py
@@ -0,0 +1,13 @@
+
+def funcion(x):
+    print("Argumento x = ", x)
+    return x*x
+
+def f(x, y):
+    return 2*x, 3*y
+
+def cuad(x):
+    """Funcion para elevar un numero al cuadrado.
+    Funciona siempre y cuando el tipo de x permita la multiplicacion.
+    """
+    return x*x
